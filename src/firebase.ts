ad79ff098495692b6e8ab9de2382e99aa723cfa0
jw <always1997@kakao.com>
2023-09-27 19:41:11 +0900

    feat: firebase auth추가

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyBp6awYepI8gwF4Z2TwWOsbBuZnM89L-oE",
+  authDomain: "xwitter-2ce81.firebaseapp.com",
+  projectId: "xwitter-2ce81",
+  storageBucket: "xwitter-2ce81.appspot.com",
+  messagingSenderId: "1090334523742",
+  appId: "1:1090334523742:web:dc504169e5430ad4dd4fcf",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
